590bf788aa6ee6026da7f158db52e3d692b06da1
broccoliass <biscuitchocolate17@gmail.com>
2022-06-06 11:09:54 +0000

    add udp server

---
+++ lab5/udpserver.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Berjaya buat sokett")
+port = 8888
+s.bind(("", port))
+print("Berjaya bind soket di port: " + str(port))
+#s.listen(5)
+print("soket tengah menunggu client!")
+while True:
+    #c, addr = s.accept()
+    print("Dapat capaian dari:" + str(addr))
+    c.send(b'Terima Kasih!')
+    buffer = c.recvfrom(1024)
+    print(buffer)
+c.close()
